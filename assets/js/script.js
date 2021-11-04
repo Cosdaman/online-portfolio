8b7e5c7b9dc44d2b3696b1892c30488c23c47558
Cosdaman <Cosdaguy@gmail.com>
2021-11-04 11:53:21 -0700

    removed some projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,20 +18,21 @@ console.log("js script connected")
 const projects = [
     {
         type: 'normal',
+        name: 'Discord Event Builder',
+        description: "This application is designed to help users with event announcements for discord.",
+        screenshotLink: 'https://user-images.githubusercontent.com/3162991/140001467-617b6057-fbe9-4f20-af6e-640697702539.png',
+        repoLink: 'https://github.com/Cosdaman/DiscordEventBuilder',
+        liveLink: 'https://cosdaman.github.io/DiscordEventBuilder/',
+    },
+    {
+        type: 'normal',
         name: 'Weather Dashboard',
         description: "This is a basic weather dashboard that is meant to showcase the developer's recently learned skills in third party web APIs.",
         screenshotLink: 'https://user-images.githubusercontent.com/3162991/138566576-b1711bf6-e2f4-42b5-bb18-547c963fa4db.png',
         repoLink: 'https://github.com/Cosdaman/BCHW6-WeatherDashboard',
         liveLink: 'https://cosdaman.github.io/BCHW6-WeatherDashboard/',
     },
-    {
-        type: 'normal',
-        name: 'Code Quiz',
-        description: "This is a basic code quiz utilizing some advanced topics of javascript.",
-        screenshotLink: 'https://user-images.githubusercontent.com/3162991/136621768-f17ce87f-166c-421b-bb3e-15c24226809c.png',
-        repoLink: 'https://github.com/Cosdaman/BCHW4-WebAPI-CodeQuiz',
-        liveLink: 'https://cosdaman.github.io/BCHW4-WebAPI-CodeQuiz/',
-    },
+
     {
         type: 'normal',
         name: 'Day Planner',
@@ -39,22 +40,6 @@ const projects = [
         screenshotLink: 'https://user-images.githubusercontent.com/3162991/137196650-13721e26-9c5a-4a7b-b15d-b88f39a52b28.png',
         repoLink: 'https://github.com/Cosdaman/BCHW5-DayPlanner',
         liveLink: 'https://cosdaman.github.io/BCHW5-DayPlanner/',
-    },
-    {
-        type: 'normal',
-        name: 'Password Generator',
-        description: "This project is aimed to demonstrate the developer's understanding of basic javascript.",
-        screenshotLink: 'https://user-images.githubusercontent.com/3162991/134982354-fec98408-e1ff-446b-b8c5-f17605205edf.png',
-        repoLink: 'https://github.com/Cosdaman/bootcampHW3-JavascriptPWGen',
-        liveLink: 'https://cosdaman.github.io/bootcampHW3-JavascriptPWGen/',
-    },
-    {
-        type: 'normal',
-        name: 'Discord Event Builder',
-        description: "This application is designed to help users with event announcements for discord.",
-        screenshotLink: 'https://user-images.githubusercontent.com/3162991/140001467-617b6057-fbe9-4f20-af6e-640697702539.png',
-        repoLink: 'https://github.com/Cosdaman/DiscordEventBuilder',
-        liveLink: 'https://cosdaman.github.io/DiscordEventBuilder/',
     },
     {
         type: 'featured',
@@ -69,7 +54,6 @@ const projects = [
 //project display initialization
 for (let i = 0; i < projects.length; i++) {
     if (projects[i].type === 'featured') {
-        console.log("featured");
         $("#featuredImg").attr("src", projects[i].screenshotLink);
         $("#featuredTitle").text(projects[i].name);
         $("#featuredDescription").text(projects[i].description);
@@ -100,4 +84,6 @@ for (let i = 0; i < projects.length; i++) {
         divEl3.append(aEl);
         divEl3.append(aEl2);
     }
+
 }
+$(".btn-secondary").attr('target', '_blank');
